9a436af933334e99ae06a95b61b3f7768c840bf6
Manveet Kaur <90703104+ManveetKaur0007@users.noreply.github.com>
2022-10-27 18:58:10 -0400

    final edit

--- assignments/src/HeatingSystem.js
+++ assignments/src/HeatingSystem.js
@@ -1,9 +1,18 @@
 import React, {useState} from "react";
+import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';  
+import Grid from '@mui/material/Grid';
+import Stack from '@mui/material/Stack';
+import Button from '@mui/material/Button';
+import {BsThermometerSnow} from "react-icons/bs";
+import {BsThermometerSun} from "react-icons/bs"
+import {BsThermometerHalf} from "react-icons/bs"
+
 /*
     React JavaScript File
     Author: Manveet Kaur
     Student Id: W0782002
-    Date: 2022/10/27
+    Date: 2022/10/24
     Filename: HeatingSystem.js
 */
 export default function HeatingSystem(){
@@ -13,39 +22,58 @@ export default function HeatingSystem(){
     const [unit, setUnit] = useState("C");
 
 
-    function convertTemp(){
-        if (unit === "C"){
-            setTemp((temp * 5/9) + 32);
-            setUnit("F");
-        }
-        if (unit === "F"){
-            setTemp((temp - 32) * 5/9);
-            setUnit("C");
-        }
-    }
-
-    return(
-    <div>
-    <h1>My Heating System</h1>
-    <h2>Set the current temperature:</h2>
-        <h2>{temp} &#176;{unit}</h2>
-        <button onClick={()=>{setTemp(temp+1)}}>+</button> &nbsp;
-        <button onClick={()=>{setTemp(temp-1)}}>-</button>
-    <hr />
-  
-    <h2>Set the mode:</h2>
-        <h2>{mode}</h2>
-        <button onClick={()=>{setMode("Cold")}}>Cold</button> &nbsp;
-        <button onClick={()=>{setMode("Heat")}}>Heat</button> &nbsp;
-        <button onClick={()=>{setMode("Auto")}}>Auto</button>
-    <hr />
-
-    <h2>Set the unit:</h2>
-        <button onClick={()=>{setUnit("C")}}>Celsius</button> &nbsp; 
-        <button onClick={()=>{setUnit("F")}}>Fahrenheit</button> 
-    <hr />
-
-    <button onClick={()=>{convertTemp()}}>Convert the Temp.</button> 
-   </div> 
-    )
+function convertTemp(){
+    if (unit === "C"){
+        setTemp(Math.round((temp * 5/9) + 32),2);
+        setUnit("F");}
+    if (unit === "F"){
+        setTemp(Math.round((temp - 32) * 5/9),2);
+        setUnit("C");}
 }
+
+return(
+<Container maxWidth="sm">
+<Box sx={{ flexGrow: 1 }}>
+    <Grid container spacing={1}>
+        <Grid item xs={12}>
+            <h1>My Heating System</h1>
+        </Grid>
+        <Grid item xs={12}>
+            <h2>Set the current temperature:</h2>
+            <h2>{temp} &#176;{unit}</h2>
+        </Grid>
+        <Grid item xs={12}>
+            <Stack spacing={1} direction="row">
+                <Button variant="outlined" onClick={()=>{setTemp(temp+1)}}>+</Button> &nbsp;
+                <Button variant="outlined" onClick={()=>{setTemp(temp-1)}}>-</Button>
+            </Stack>
+        </Grid>
+        <Grid item xs={12}>
+            <h2>Set the mode:</h2>
+            <h2>{mode}</h2>
+        </Grid>
+        <Grid item xs={12}>
+            <Stack spacing={1} direction="row">
+                <Button variant="outlined" onClick={()=>{setMode("Cold")}}><BsThermometerSnow /></Button> &nbsp;
+                <Button variant="outlined" onClick={()=>{setMode("Heat")}}><BsThermometerSun /></Button> &nbsp;
+                <Button variant="outlined" onClick={()=>{setMode("Auto")}}><BsThermometerHalf/></Button>
+            </Stack>
+        </Grid>
+        <Grid item xs={12}>
+            <h2>Set the unit:</h2>
+        </Grid>
+        <Grid item xs={12}>
+            <Stack spacing={1} direction="row">
+                <Button variant="outlined" onClick={()=>{setUnit("C")}}>Celsius</Button> &nbsp; 
+                <Button variant="outlined" onClick={()=>{setUnit("F")}}>Fahrenheit</Button>
+            </Stack>
+        </Grid>
+        <Grid item xs={12}>
+            <Stack spacing={1} direction="row">
+                <Button variant="contained" onClick={()=>{convertTemp()}}>Convert the Temp.</Button> 
+            </Stack>
+        </Grid> 
+    </Grid>
+</Box>
+</Container> 
+)}
