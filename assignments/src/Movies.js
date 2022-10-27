022bb3a8b950bdb847bf00bd4774cae1e622fa04
Manveet Kaur <90703104+ManveetKaur0007@users.noreply.github.com>
2022-10-27 19:54:33 -0400

    intial code

---
+++ assignments/src/Movies.js
@@ -0,0 +1,21 @@
+import React, {useState} from "react";
+/*
+    React JavaScript File
+    Author: Manveet Kaur
+    Student Id: W0782002
+    Date: 2022/10/26
+    Filename: Movies.js
+*/
+export default function Movies(){
+    return(
+        <div>
+            <hr/>
+            <h3>Movie Name</h3>
+            <h3>Year</h3>
+            <h3>Genre</h3>
+            <h3>Director</h3>
+            <h3>Movie Age</h3>
+            <hr />
+        </div>
+    )
+}
